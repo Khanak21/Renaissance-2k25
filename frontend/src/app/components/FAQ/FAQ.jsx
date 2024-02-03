080fdd573cd7981b597f6c37453cb6a170cbb906
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-02-03 18:58:14 +0530

    minor changes in faq.jsx

--- frontend/src/app/components/FAQ/FAQ.jsx
+++ frontend/src/app/components/FAQ/FAQ.jsx
@@ -1,5 +1,5 @@
-import React, { useState,useEffect } from "react";
-import { motion, AnimatePresence  ,useAnimation } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const faqData = [
@@ -77,39 +77,40 @@ const FAQ = () => {
                 variants={item}
                 className="bg-gray-100 p-6 rounded-lg shadow-md overflow-hidden transition-all duration-300 ease-in-out max-h-full"
               >
-                
-                  <h3
-                    className={`text-xl font-semibold cursor-pointer ${
-                      selectedQuestion === index ? "text-blue-500" : "text-black"
-                    }`}
-                    onClick={() => handleToggle(index)}
-                  >
-                    <div className="flex justify-between">
+                <h3
+                  className={`text-xl font-semibold cursor-pointer ${
+                    selectedQuestion === index ? "text-blue-500" : "text-black"
+                  }`}
+                  onClick={() => handleToggle(index)}
+                >
+                  <div className="flex justify-between">
                     {faq.question}
                     <motion.div
                       initial={{ rotate: 0 }}
                       animate={
-                        selectedQuestion === index ? { rotate: -90 } : { rotate: 0 }
+                        selectedQuestion === index
+                          ? { rotate: -90 }
+                          : { rotate: 0 }
                       }
                       className="m-2"
                     >
-                      <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        width="16" height="16" fill="currentColor" 
-                        class="bi bi-chevron-down" 
-                        viewBox="0 0 16 16">
-                          <path 
-                          fill-rule="evenodd" 
-                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-chevron-down"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                        />
                       </svg>
-                      
-                     
                     </motion.div>
-                   
-                    </div>
-                  </h3>
-                  
-                
+                  </div>
+                </h3>
+
                 <AnimatePresence>
                   {selectedQuestion === index && (
                     <motion.p
