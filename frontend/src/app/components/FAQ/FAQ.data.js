4efcbcc3d8f54905ed11f9fd1961b8306bc20001
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-02-06 02:17:12 +0530

    changed langing , faq , gallery component
    - changed video to 2k22
    - added actual mnnit gallery images
    - added faq data

---
+++ frontend/src/app/components/FAQ/FAQ.data.js
@@ -0,0 +1,41 @@
+export const faqData = [
+  {
+    question: "What is the mode of conduct of Renaissance 7.0?",
+    answer: "This event will be held in offline mode",
+  },
+  {
+    question: "Is there any registration fee for the event?",
+    answer:
+      "The event has no registration fees for the MNNIT students. However, the students of other institutes need to pay a registration fee of Rs.149 .",
+  },
+  {
+    question: "What is the team size for the events?",
+    answer:
+      "Different events have different team sizes, and you can refer to the particular event section for the required team size.",
+  },
+  {
+    question: "Is there any new event this time in Renaissance 8.0?",
+    answer:
+      "Yes, CONTENT-O-RAMA and Nayak are the new events introduced this year.",
+  },
+  {
+    question: "How can I register for the events?",
+    answer:
+      "You can register for the events on our website by creating an account.",
+  },
+  {
+    question: "Can I register for multiple events?",
+    answer:
+      "Yes, participation in more than one event is allowed.",
+  },
+  {
+    question: "Where can I watch the events live?",
+    answer:
+      "The live links for each event will be shared before the event starts.",
+  },
+  {
+    question: "Refund and Cancellation Policy?",
+    answer:
+      "You may choose to not participate in any of the events. However, no refund will be issued in case of cancellation.",
+  },
+];
