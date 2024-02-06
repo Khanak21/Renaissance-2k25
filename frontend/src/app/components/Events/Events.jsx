020753c793dd7b5f8aa6d6d723b2c52f7a227fac
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-02-07 00:01:01 +0530

    removed animation from events components

--- frontend/src/app/components/Events/Events.jsx
+++ frontend/src/app/components/Events/Events.jsx
@@ -2,71 +2,31 @@
 
 import React, { useEffect, useState } from "react";
 import EventCard from "./EventCard";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 import getAllEventsApi from "../../../api/getAllEvents.api";
 
 const Events = () => {
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.2,
-        staggerChildren: 0.1,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-    },
-  };
-
-  const controls = useAnimation();
-  const [ref, inView] = useInView({ triggerOnce: true });
   const [EventsData, setEventsData] = useState([]);
 
   useEffect(() => {
     getAllEventsApi().then((data) => {
-      console.log(data);
       if (data.success) {
         setEventsData(data.data);
       }
     });
   }, []);
 
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    }
-  }, [controls, inView]);
-
   return (
     <>
-      <motion.div
-        id="events"
-        className="w-full"
-        variants={container}
-        initial="hidden"
-        animate={controls}
-        ref={ref}
-      >
+      <div id="events" className="w-full">
         <div className="px-10 py-10 mx-auto bg-black sm:p-8 md:p-12">
           <div className="flex items-center justify-center pb-12 text-lg font-extrabold sm:pb-14 sm:text-xl lg:pb-16 lg:text-2xl text-[#ffde89]">
             OUR EVENTS
           </div>
           <div className="grid grid-cols-1 gap-x-5 gap-y-10 sm:grid-cols-2 lg:flex lg:flex-row lg:flex-wrap lg:justify-center">
-            {/* <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"> */}
             {EventsData.map((e, index) => (
-              <motion.div
+              <div
                 className="relative font-bold min-h-[20em] group rounded-3xl hover:border-dashed hover:border-2 hover:border-[#ffde89] bg-[#27282a] shadow-lg lg:w-[20%]"
                 key={index}
-                variants={item}
               >
                 <EventCard
                   id={index}
@@ -75,11 +35,11 @@ const Events = () => {
                   route={e.route}
                   svg={e.svg}
                 />
-              </motion.div>
+              </div>
             ))}
           </div>
         </div>
-      </motion.div>
+      </div>
     </>
   );
 };
