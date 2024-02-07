d089a7d5844a787d049b8ebf9ac2e083aa3dbd8c
samjain233 <94921996+samjain233@users.noreply.github.com>
2024-02-08 03:54:40 +0530

    added more images in gallery

--- frontend/src/app/components/Gallery/Photos.js
+++ frontend/src/app/components/Gallery/Photos.js
@@ -7,12 +7,12 @@ const unsplashPhotos = [
   { url: "/images/Gallery/gallery4.jpeg", width: 919, height: 869 },
   { url: "/images/Gallery/gallery5.jpeg", width: 1096, height: 860 },
   { url: "/images/Gallery/gallery6.jpeg", width: 1280, height: 723 },
-  { url: "/images/Gallery/gallery1.jpeg", width: 1280, height: 781 },
-  { url: "/images/Gallery/gallery2.jpeg", width: 1110, height: 858 },
-  { url: "/images/Gallery/gallery3.jpeg", width: 996, height: 862 },
-  { url: "/images/Gallery/gallery4.jpeg", width: 919, height: 869 },
-  { url: "/images/Gallery/gallery5.jpeg", width: 1096, height: 860 },
-  { url: "/images/Gallery/gallery6.jpeg", width: 1280, height: 723 },
+  { url: "/images/Gallery/gallery7.jpg", width: 1280, height: 719 },
+  { url: "/images/Gallery/gallery8.jpg", width: 4032, height: 3024 },
+  { url: "/images/Gallery/gallery9.jpg", width: 5143, height: 4000 },
+  { url: "/images/Gallery/gallery10.jpg", width: 5999, height: 3820 },
+  { url: "/images/Gallery/gallery11.jpg", width: 5510, height: 3858 },
+  { url: "/images/Gallery/gallery12.jpg", width: 5836, height: 2795 },
 ];
 
 const photos = unsplashPhotos.map((photo) => ({
