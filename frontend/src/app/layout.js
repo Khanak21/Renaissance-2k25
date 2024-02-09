16233736a37a58b2f8e8ac36d92792f1c56fc0f0
Neeraj Chatterjee <neeraj.20204123@mnnit.ac.in>
2024-02-09 17:01:18 +0530

    Changed the metadata of HTML page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,9 +5,9 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Renaissance 2k24",
+  title: "Renaissance 2024 | The Annual MNNIT E-Summit",
   description:
-    "Renaissance is the annual entrepreneurial summit of MNNIT Allahabad that provides a platform for aspiring entrepreneurs to encourage the entrepreneurial journey of translating thoughts into action. Renaissance includes a gamut of activities all of which provide an avenue to harbor entrepreneurial skills and stimulate discussion on ideas that possess the potential to turn the issues that plague us into opportunities. This year it is going to held on 10-11 feb",
+    "Renaissance is the annual entrepreneurial summit of MNNIT Allahabad that provides a platform for aspiring entrepreneurs to encourage the entrepreneurial journey of translating thoughts into action. Renaissance includes a gamut of activities all of which provide an avenue to harbor entrepreneurial skills and stimulate discussion on ideas that possess the potential to turn the issues that plague us into opportunities. This year it is going to be held on 10 to 11 Feb",
 };
 
 export default function RootLayout({ children }) {
